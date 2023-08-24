6b10386ecec6880f37221635932be7f33e415cc3
velingkar <49779663+velingkar@users.noreply.github.com>
2023-08-24 11:34:47 +0530

    Init Config file
    put this file in directory for t-shirt export

---
+++ config.py
@@ -0,0 +1,32 @@
+# Always changed variables
+category = "Medieval Wonders"
+sku_prefix = "KTMDW"
+handle_prefix	= "medieval-wonders-"
+tags = "Medieval Wonders"
+body = "<ul>\
+        <li>4-5 working days dispatch time.</li>\
+        <li>100% Organic Cotton Biowash preshrunk T-shirt.</li>\
+        <li>8 colors in premium organic cotton fabric.</li>\
+        <li>5 size wide range, relaxed fit.</li>\
+        <li>Eco-friendly printing as well as not harmful to the nature.</li>\
+        <li>Regular Fit best for casual wear.</li>\
+        <li>180 GSM fabric which is breathable and lightweight.</li>\
+        <li>100% eco-friendly packaging with seed paper hang tag.</li>\
+    </ul>"
+
+# initialise key swatches
+sizes = ["4-5 Years","6-7 Years","7-8 Years","9-10 Years"]
+colors = ["Red", "Blue", "Light Blue", "Pink", "Yellow"]
+imageColors = ["Red", "Blue", "LightBlue", "Pink", "Yellow", "Navy"]
+
+# initialise constants - these are rarely changed
+weight = 250
+cost =	400
+price =	899
+compare_price =	899
+variant_qty = 0
+image_path = "https://cdn.shopify.com/s/files/1/0801/1258/2976/files/"
+product = "Premium Kids's T-Shirt"
+vendor = "My Store"
+product_category = "Apparel & Accessories > Clothing > Shirts & Tops"
+Product_type = "kids causual tshirts"
