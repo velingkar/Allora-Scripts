bf016ad11a7524d04074a6d3b7b5025281d2636f
velingkar <49779663+velingkar@users.noreply.github.com>
2023-08-24 15:29:23 +0530

    BetterSKULogic
    created SKU as follows:
    - Product Category (2)
    - Collection (3)
    - FileName(4) - 3 letters, 1 number
    - Color(2)
    - Variant(2)

--- ExportToShopify.py
+++ ExportToShopify.py
@@ -15,6 +15,48 @@ def GetSKUImageName(mediaFiles, fileName, sku_prefix, imageColors, colorIndex):
         print (Fore.LIGHTMAGENTA_EX+"WARNING: SKU Media File Not Found "+ mediaName)
         return ""
 
+# Get Unique 4 letter representation based on design file name
+# first 3 letters are unique letters from filename, last position is for a number to break ties
+# example - FAI1
+def GetUniqueFileSKU(files):
+    fileSKUs = []
+    for file in files:
+        fileSKU = ""
+        words = file.split(" ")
+        if (len(words) > 2):
+            fileSKU = words[0][0] + words[1][0] + words[2][0]; # get first letter each from first 3 words
+        elif (len(words) > 1):
+            fileSKU = words[0][0:2] + words[1][0]; # 2 letters from first word, 1 letter from second word
+        else:
+            fileSKU = file[0:3]; # first 3 letters
+
+        # let us make string upper case
+        fileSKU = fileSKU.upper()
+
+        # sanity check
+        if (len(fileSKU) != 3):
+            print (Fore.LIGHTMAGENTA_EX+"WARNING: Could not get 3 words for SKU. ",file," : ", fileSKU)
+            fileSKUs.append(str(random.randint(100,999))) # trying something random
+            continue #move to next file
+
+        # let us check for uniqueness (append 1,2,3,4... to break ties)
+        for index in range(1,9):
+            if (not fileSKU+str(index) in fileSKUs):
+                fileSKUs.append(fileSKU+str(index))
+                break # exit the loop
+            if (index >= 9):
+                print (Fore.LIGHTMAGENTA_EX+"WARNING: Too Many Files with Same SKU Name Found. ",file)
+                fileSKUs.append(fileSKU[0]+str(random.randint(10,99))) # trying something random
+    
+    # return the array
+    if (len(files) != len(fileSKUs)):
+        print(Fore.LIGHTMAGENTA_EX+"ERROR: Expected - Files and FileSkus lengths should match. ")
+        print(files)
+        print(fileSKUs)
+    
+    # done
+    return fileSKUs
+
 # Start Prompt
 Style.RESET_ALL
 print(Back.YELLOW)
@@ -45,7 +87,10 @@ try:
     # import key variables
     sys.path.append(folder) #temporarily adds folder to system path
     import settings
-    numColumns = len(settings.headers)
+
+    # initiatize CSV headers
+    headers = ["Handle","Title","Body (HTML)","Vendor","Product Category","Type","Tags","Published","Option1 Name","Option1 Value","Option2 Name","Option2 Value","Option3 Name","Option3 Value","Variant SKU","Variant Grams","Variant Inventory Tracker","Variant Inventory Qty","Variant Inventory Policy","Variant Fulfillment Service","Variant Price","Variant Compare At Price","Variant Requires Shipping","Variant Taxable","Variant Barcode","Image Src","Image Position","Image Alt Text","Gift Card","SEO Title","SEO Description","Google Shopping / Google Product Category","Google Shopping / Gender","Google Shopping / Age Group","Google Shopping / MPN","Google Shopping / Condition","Google Shopping / Custom Product","Google Shopping / Custom Label 0","Google Shopping / Custom Label 1","Google Shopping / Custom Label 2","Google Shopping / Custom Label 3","Google Shopping / Custom Label 4","Variant Image","Variant Weight Unit","Variant Tax Code","Cost per item","Included / India","Included / International","Price / International","Compare At Price / International","Status"]
+    numColumns = len(headers)
 
     # Get the file names in the design folder
     files = os.listdir(folder+"\designs")
@@ -81,9 +126,10 @@ try:
         writer = csv.writer(f)
 
         # write CSV header
-        writer.writerow(settings.headers)
+        writer.writerow(headers)
 
         # Loop through the files and add the prefix
+        fileSKUs = GetUniqueFileSKU(files)
         for fileIndex, file in enumerate(files):
             # get the file name without extension
             fileName = os.path.splitext(file)
@@ -93,7 +139,6 @@ try:
                 continue
 
             # for every design, shuffle the image position index (variety of colors will be shown)
-            # TODO This did not work - need to get this ahead of time 
             random.shuffle(colorPosition)
 
             #make unique handle
@@ -137,8 +182,8 @@ try:
                     row[0] = handle
                     row[9] = color
                     row[11] = size
-                    # create SKU Name (prefix(5) + design(3) + color(2) + size(2) )
-                    row[14] = settings.sku_prefix + f'{(fileIndex+1-skippedFileCount):03}' + f'{(colorIndex+1):02}' + f'{(sizeIndex+1):02}'    
+                    # create SKU Name (prefix(5) + design(4) + color(2) + size(2) )
+                    row[14] = settings.sku_prefix + fileSKUs[fileIndex] + f'{(colorIndex+1):02}' + f'{(sizeIndex+1):02}'    
                     row[15] = settings.weight
                     row[17] = settings.variant_qty
                     row[18] = "deny" #variant policy
